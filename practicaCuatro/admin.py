dbcc439c3e7bafbd77558b5ecf3ee0a1410c3af3
Alex <abarrosoca.inf@upsa.es>
2025-04-02 14:00:25 +0200

    Reotques finales css y finalizacion de la practica

--- practicaCuatro/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Biblioteca, Libro, Usuario, Prestamo
-admin.site.register(Biblioteca)
-admin.site.register(Libro)
-admin.site.register(Usuario)
-admin.site.register(Prestamo)
-
-

4a1afe8f19766c0f6cf53102bc41d6c832e8d928
alexbarroso <alebarzal@gmail.com>
2025-04-03 08:19:41 +0200

    Retoques finales css y finalizacion de la practica

--- practica4/urls.py
+++ practica4/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('practica4Formularios/', include('practica4Formularios.urls')),
+
+    path('', RedirectView.as_view(url='/practica4Formularios/', permanent=False)),
 ]
